1430874effb4f0cf1bfd1b3670bf12eecb44c7f8
Luckman21 <luckmaan21@gmail.com>
2024-06-23 14:50:20 -0400

    thread.c Compiles
    Refactored the code to work with pthread.h, using join and create.  Next steps are to debug, then switch to using a non-blocking join.

--- EECS_3221_E_A1/thread.c
+++ EECS_3221_E_A1/thread.c
@@ -13,7 +13,7 @@ YorkU email address: luq21@my.yorku.ca
     //dif = max - min
 
 //Output for each thread
-    //thread_name SUM DIF MIN MAX
+    //child_name SUM DIF MIN MAX
 //Output at the end
     //MINIMUM MAXIMUM
 
@@ -21,31 +21,33 @@ YorkU email address: luq21@my.yorku.ca
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+//#include <sys/wait.h>
 #include <pthread.h>
 
 typedef struct thread {
-    char name[50]; //Thread name (name of the dataset it's assigned to)
-    int tid;    //Thread ID
-    int min;    //Minimum value of the dataset
-    int max;    //Maximum value of the dataset
-    int sum;    //SUM = Max + Min
-    int dif;    //DIF = Max - Min
-}ThreadObj;
+    char name[50];  //Thread name (name of the dataset it's assigned to)
+    int index;      //
+    double sum;     //SUM = Min + Max
+    double dif;     //DIF = Min - Max
+}Thread;
 
-typedef struct HashtableNode {
-    char value[50]; //Name of the dataset
-    int key;        //PID
-}Hashtable;
+/**
+ * @brief Function that runs as a thread when called using pthread_create.  
+ * It computes the sum and dif between the max and min values of the dataset.
+ * 
+ * @param param 
+ * @param index The index assigned to this thread from the threads struct array.
+ * @param t The thread struct array index passed as a pointer, so the thread can write to shared heap memory.
+ * @return void* 
+ */
+
+void *thread_function(void *param);
 
 int main(int argc, char* argv[]) {
 
-    FILE* dataset;
-
-    //Allocate heap memory for a process struct (each child process can manipulate their own copy of this struct)
-    ThreadObj *t = malloc(sizeof(ThreadObj));
-
-    //Create an array of Hashtable Node elements to store PID as the key and dataset name as the value
-    Hashtable *hash[] = malloc(sizeof(Hashtable) * (argc - 1));
+    pthread_t workers[argc - 1];    //Create the number of threads based on the number of datasets input
+    Thread t_mem[argc - 1];         //Allocate heap memory for threads to store their memory to (heap shared between threads, create a struct instance)
 
     /*
     Create n threads for n datasets using argc and argv
@@ -54,37 +56,96 @@ int main(int argc, char* argv[]) {
      - argv is a "string" (char[]) array of each element in the CLI
     */
     for (int i = 1; i < argc; i++) {
-        //Create a thread
-
-        //Add TID to a Hashtable to track the list of threads
-        hash[i-1]->key = t->tid;
-        *hash[i-1]->value = argv[i];
+        strcpy(t_mem[i-1].name, argv[i]);
+        t_mem[i-1].index = i-1;
+        pthread_create(&workers[i-1], NULL, thread_function, &t_mem[i-1]);
+        pthread_join(workers[i-1], NULL);
     }
 
-    //Main thread of execution
-    for (int i = 0; i < argc - 1; i++) {
-        //Upon thread completion
-        
-        //Variables to track the current child process that has finished executing
-        int* child_tid;
-        char child_name[50];
+    double maxF, minF = 0;
 
-        //Print: name SUM=sum DIF=dif MIN=min MAX=max
-        //printf("%s SUM=%d DIF=%d MIN=%d MAX=%d", p->name, p->sum, p->dif, p->max);
+    for (int i = 0; i < argc - 1; i++) {
+
+        double max, min, sum, dif;
+        Thread *tx = malloc(sizeof(Thread));
+        
+        //Wait for a thread to finish execution
+
+        /*Calculate max and min (use double precision)
+            max = (sum + dif) / 2
+            min = (sum - dif) / 2
+        Print: name SUM=sum DIF=dif MIN=min MAX=max
+        printf("%s SUM=%d DIF=%d MIN=%d MAX=%d", p->name, p->sum, p->dif, p->max);*/
+
+        sum = tx->sum;
+        dif = tx->dif;
+
+        max = ((sum + dif) / (double)(2));
+        min = ((sum - dif) / (double)(2));
         
         if (i == 0) {
             //Set min and max values to the values from the first process to finish execution
+            minF = min;
+            maxF = max;
         }
 
         else {
             //If child min < parent min | parent min = child min
+            if (min < minF)
+                minF = min;
 
             //If child max > parent max | parent max = child max
+            if (max > maxF)
+                maxF = max;
         }
+
+        //Print all the significant values for the child process
+        printf("%s SUM=%lf DIF=%lf MIN=%lf MAX=%lf\n", tx->name, sum, dif, min, max);
+        
+        free(tx);  //Free memory from the heap
     }
-    //All children finished executing
-        //Output MAXIMUM and MINIMUM
-    printf("MINIMUM=%d MAXIMUM=%d", t->min, t->max);
+    /*All children finished executing
+        Output MAXIMUM and MINIMUM*/
+    printf("MINIMUM=%lf MAXIMUM=%lf\n", minF, maxF);
 
     return 0;
+}
+
+void *thread_function(void *param) {
+
+    FILE* dataset;
+    double num;
+    double min;
+    double max;
+    int first = 1;
+
+    Thread *t = malloc(sizeof(Thread));
+    t = param;
+
+    dataset = fopen(t->name,"r");   //Read data from dataset n file
+
+    //For loop to parse data, runs until end of file
+    while (fscanf(dataset, "%lf", &num) != EOF) {
+
+        //Set the first max and min to actual values of the dataset instead of having them initialized with garbage values
+        if (first == 1) {
+            min = num;
+            max = num;
+            first = 0;
+        }
+
+        if (num < min)
+            min = num;
+
+        if (num > max)
+            max = num;
+    }
+
+    fclose(dataset);    //Close file after using
+
+    //Calculate sum and dif
+    t->sum = min + max;
+    t->dif = min - max;
+
+    return NULL;
 }
